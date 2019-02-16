626a15cf7da731f41543574204843320792a445b
dcruwys <dcruwys@gmail.com>
2019-02-16 15:03:54 -0700

    Added comment functionality

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/'))
 
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
+  app.log(web3.utils.fromAscii('dao-git/dao-git-test'))
 
   app.on('pull_request.opened', async context => {
-      var bodyComment = "I am writing test msgs to test:"
-
-      //HARDCODED TRASH
-      const owner = 'dao-git';
-      const repo = 'dao-git-test';
+      var bodyComment = "Hi \n"
+      var pull_request_id = context.payload.pull_request.number
+      var repo_id = context.payload.pull_request.head.repo.full_name
+      app.log(repo_id)
+      var hex_repo_id = web3.utils.fromAscii(repo_id);
+      var split_repo_id = repo_id.split('/')
+      const owner = split_repo_id[0]
+      const repo = split_repo_id[1]
+      app.log(hex_repo_id)
       const anon = 0; //We dont want to out anonymous contributors
-      //
+
       const  contributors = await context.github.repos.listContributors({
         owner, repo, anon
-      }).then(({data, status, headers}) => {
-        app.log(data)
-        for(var contributor in data)){
-          app.log(contributor)
-          app.log(contributor.login)
-          app.log(JSON.parse(contributor))
-        }
+      }).then(({data}) => {
+        data.map(contributor => {
+          bodyComment += "@"+contributor.login+" "
+        })
       })
-      // app.log(contributors.data)
-      // for (var contributor in contributors.data) {
-      //   app.log(contributor)
-      //   contributor_obj = JSON.parse(contributor)
-      //   app.log(contributor_obj)
-      //   bodyComment += ""+contributor_obj.login+" "
-      // }
+      bodyComment += "\n please vote at https://oneclickdapp.com/status-picnic/ \n with *pull request id*: "
+      +pull_request_id
+      +"\n and *repo id*: "
+      + hex_repo_id
+
       const comment = context.issue({body: bodyComment})
       return context.github.issues.createComment(comment)
   })
-
-
-
-  //app.on('issues.opened', async context => {
-  //  const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-  //  return context.github.issues.createComment(issueComment)
-  //})
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
