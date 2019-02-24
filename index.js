cad4d44ba6056c7841ec62617c1e8e4cb3b0f3b3
Patrick Gallagher <blockchainbuddha@gmail.com>
2019-02-23 22:36:50 -0500

    added check for repo init

--- index.js
+++ index.js
@@ -1,85 +1,131 @@
-const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/'))
-const contract_address = '0xD9A37024b41f0c13cF85eeADcaE257181b965da3';
+const Web3 = require("web3");
+const web3 = new Web3(
+  new Web3.providers.HttpProvider("https://rinkeby.infura.io/")
+);
+const contract_address = "0xD9A37024b41f0c13cF85eeADcaE257181b965da3";
+contract_interface = [
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_pullRequestId",
+        type: "uint256"
+      }
+    ],
+    name: "isPassing",
+    outputs: [
+      {
+        name: "",
+        type: "bool"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "",
+        type: "bytes32"
+      }
+    ],
+    name: "repo",
+    outputs: [
+      {
+        name: "manager",
+        type: "address"
+      },
+      {
+        name: "passingThreshold",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  }
+];
 
 module.exports = app => {
-  app.log('Yay, the app was loaded!')
+  app.log("Yay, the app was loaded!");
 
-  app.on(['pull_request.opened', 'pull_request.reopened'], async context => {
-      var bodyComment = "Hello! \n"
-      var pull_request_id = context.payload.pull_request.number
-      var repo_id = context.payload.repository.full_name
-      app.log(repo_id)
-      var hex_repo_id = web3.utils.fromAscii(repo_id);
-      var split_repo_id = repo_id.split('/')
-      const owner = split_repo_id[0]
-      const repo = split_repo_id[1]
-      app.log(hex_repo_id)
-      const anon = 0; //We dont want to out anonymous contributors
-      const  contributors = await context.github.repos.listContributors({
-        owner, repo, anon
-      }).then(({data}) => {
-        data.map(contributor => {
-          bodyComment += "@"+contributor.login+" "
-        })
-      })
-      bodyComment += "\n please vote [here](https://dao-git.github.io/front-end/"
-                  +"?repo="
-                  +hex_repo_id
-                  +"&pr="
-                  +pull_request_id
-                  +"&contract="
-                  +contract_address
-                  +")."
-      const comment = context.issue({body: bodyComment})
-      return context.github.issues.createComment(comment)
-  })
-
-  app.on('issue_comment.created', async context => {
- 
-    var contract = new web3.eth.Contract(
-	[{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "_repoId",
-				"type": "bytes32"
-			},
-			{
-				"name": "_pullRequestId",
-				"type": "uint256"
-			}
-		],
-		"name": "isPassing",
-		"outputs": [
-			{
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	}
-], contract_address);
-    app.log(contract_address)
-    var pull_request_id = context.payload.issue.number
-    var repo_id = context.payload.repository.full_name
+  app.on(["pull_request.opened", "pull_request.reopened"], async context => {
+    var repo_id = context.payload.repository.full_name;
     var hex_repo_id = web3.utils.fromAscii(repo_id);
-    contract.methods.isPassing(hex_repo_id, pull_request_id).call().then(result => {
-      if(result){
-        app.log(result)
-        var split_repo_id = repo_id.split('/')
-        const owner = split_repo_id[0]
-        const repo = split_repo_id[1]
-        const base = 'master'
-        const number = pull_request_id
-        context.github.pullRequests.merge({
-          owner,
-          repo,
-          number
-        })
-      }
-    })
-  })
-}
+    var contract = new web3.eth.Contract(contract_interface, contract_address);
+    contract.methods
+      .repo(hex_repo_id)
+      .call()
+      .then(result => {
+        if (result) {
+          app.log(result);
+          // Check if repo has been initialized
+          if (result[1] > 0) {
+            var bodyComment = "Hello contributors! \n";
+            var pull_request_id = context.payload.pull_request.number;
+            app.log(repo_id);
+            var split_repo_id = repo_id.split("/");
+            const owner = split_repo_id[0];
+            const repo = split_repo_id[1];
+            app.log(hex_repo_id);
+            const anon = 0; //We dont want to out anonymous contributors
+            const contributors = await context.github.repos
+              .listContributors({
+                owner,
+                repo,
+                anon
+              })
+              .then(({ data }) => {
+                data.map(contributor => {
+                  bodyComment += "@" + contributor.login + " ";
+                });
+              });
+            bodyComment +=
+              "\n please vote [here](https://dao-git.github.io/front-end/" +
+              "?repo=" +
+              hex_repo_id +
+              "&pr=" +
+              pull_request_id +
+              "&contract=" +
+              contract_address +
+              ").";
+            const comment = context.issue({ body: bodyComment });
+            return context.github.issues.createComment(comment);
+          }
+        }
+      });
+    return;
+  });
+
+  app.on("issue_comment.created", async context => {
+    var contract = new web3.eth.Contract(contract_interface, contract_address);
+    app.log(contract_address);
+    var pull_request_id = context.payload.issue.number;
+    var repo_id = context.payload.repository.full_name;
+    var hex_repo_id = web3.utils.fromAscii(repo_id);
+    contract.methods
+      .isPassing(hex_repo_id, pull_request_id)
+      .call()
+      .then(result => {
+        if (result) {
+          app.log(result);
+          var split_repo_id = repo_id.split("/");
+          const owner = split_repo_id[0];
+          const repo = split_repo_id[1];
+          const base = "master";
+          const number = pull_request_id;
+          context.github.pullRequests.merge({
+            owner,
+            repo,
+            number
+          });
+        }
+      });
+  });
+};
