3645de3070592f73af518ed96658e51af290e599
David Conroy <dconroy@gmail.com>
2019-02-24 00:50:39 -0500

    Changes Bot Message When Repo Not Initialized (#40)
    * added check for repo init
    
    * added comment when repo has not been set up yet
    
    * fix await issue
    
    * log out comment
    
    * add logs
    
    * check initialization first
    
    * simplifying
    
    * trailing bracket
    
    * return comment regardless
    
    * adding logs
    
    * trying to hardcode result param
    
    * more logging
    
    * trailing quote
    
    * fixing async
    
    * move hex_repo_id
    
    * additional logs
    
    * removing deep equal
    
    * adding links for initilization
    
    * formatting
    
    * additonal formatting

---
+++ contracts/ABI.js
@@ -0,0 +1,230 @@
+[
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_pullRequestId",
+        type: "uint256"
+      }
+    ],
+    name: "isPassing",
+    outputs: [
+      {
+        name: "",
+        type: "bool"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_pullRequestId",
+        type: "uint256"
+      }
+    ],
+    name: "votesNeeded",
+    outputs: [
+      {
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_passingThreshold",
+        type: "uint256"
+      }
+    ],
+    name: "createRepo",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_address",
+        type: "address"
+      }
+    ],
+    name: "revokeWhitelist",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_pullRequestId",
+        type: "uint256"
+      }
+    ],
+    name: "approvePullRequest",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "",
+        type: "bytes32"
+      }
+    ],
+    name: "repo",
+    outputs: [
+      {
+        name: "manager",
+        type: "address"
+      },
+      {
+        name: "passingThreshold",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_address",
+        type: "address"
+      }
+    ],
+    name: "whitelist",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_address",
+        type: "address"
+      }
+    ],
+    name: "isWhiteListed",
+    outputs: [
+      {
+        name: "",
+        type: "bool"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "source",
+        type: "string"
+      }
+    ],
+    name: "stringToBytes32",
+    outputs: [
+      {
+        name: "result",
+        type: "bytes32"
+      }
+    ],
+    payable: false,
+    stateMutability: "pure",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_pullRequestId",
+        type: "uint256"
+      }
+    ],
+    name: "getVotes",
+    outputs: [
+      {
+        name: "",
+        type: "address[]"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        indexed: false,
+        name: "_pullRequestId",
+        type: "uint256"
+      },
+      {
+        indexed: false,
+        name: "passing",
+        type: "bool"
+      }
+    ],
+    name: "approved",
+    type: "event"
+  }
+];
--- index.js
+++ index.js
@@ -1,84 +1,147 @@
-const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/'))
-const contract_address = '0xD9A37024b41f0c13cF85eeADcaE257181b965da3';
+const Web3 = require("web3");
+const web3 = new Web3(
+  new Web3.providers.HttpProvider("https://rinkeby.infura.io/")
+);
+const contract_address = "0xD9A37024b41f0c13cF85eeADcaE257181b965da3";
+contract_interface = [
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "_repoId",
+        type: "bytes32"
+      },
+      {
+        name: "_pullRequestId",
+        type: "uint256"
+      }
+    ],
+    name: "isPassing",
+    outputs: [
+      {
+        name: "",
+        type: "bool"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "",
+        type: "bytes32"
+      }
+    ],
+    name: "repo",
+    outputs: [
+      {
+        name: "manager",
+        type: "address"
+      },
+      {
+        name: "passingThreshold",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  }
+];
 
 module.exports = app => {
-  app.log('Yay, the app was loaded!')
+  app.log("Yay, the app was loaded!");
 
-  app.on(['pull_request.opened', 'pull_request.reopened'], async context => {
-    var bodyComment = 'Hello, \n'
-    var pullRequestId = context.payload.pull_request.number
-    var repoId = context.payload.repository.full_name
-    app.log(repoId)
-    var hexRepoId = web3.utils.fromAscii(repoId)
-    var splitRepoId = repoId.split('/')
-    const owner = splitRepoId[0]
-    const repo = splitRepoId[1]
-    app.log(hexRepoId)
-    const anon = 0 // We dont want to out anonymous contributors
-    await context.github.repos.listContributors({
-      owner, repo, anon
-    }).then(({ data }) => {
-      data.map(contributor => {
-        bodyComment += '@' + contributor.login + ' '
+  app.on(["pull_request.opened", "pull_request.reopened"], async context => {
+    //gather contributors
+    var repo_id = context.payload.repository.full_name;
+    var hex_repo_id = web3.utils.fromAscii(repo_id);
+    var bodyComment = "Hello contributors! \n";
+    var pull_request_id = context.payload.pull_request.number;
+    var split_repo_id = repo_id.split("/");
+    const owner = split_repo_id[0];
+    const repo = split_repo_id[1];
+    const anon = 0; //We dont want to out anonymous contributors
+    const contributors = await context.github.repos
+      .listContributors({
+        owner,
+        repo,
+        anon
       })
-
-      bodyComment += "\n please vote [here](https://dao-git.github.io/front-end/"
-                  +"?repo="
-                  +hex_repo_id
-                  +"&pr="
-                  +pull_request_id
-                  +"&contract="
-                  +contract_address
-                  +")."
-      const comment = context.issue({body: bodyComment})
-      return context.github.issues.createComment(comment)
-  })
-
-  app.on('issue_comment.created', async context => {
- 
-    var contract = new web3.eth.Contract(
-      [{
-        'constant': true,
-        'inputs': [
-          {
-            'name': '_repoId',
-            'type': 'bytes32'
-          },
-          {
-            'name': '_pullRequestId',
-            'type': 'uint256'
+      .then(({ data }) => {
+        data.map(contributor => {
+          if (contributor.login !='dao-git-probot[bot]'){
+          bodyComment += "@" + contributor.login + " ";
           }
-        ],
-        'name': 'isPassing',
-        'outputs': [
-          {
-            'name': '',
-            'type': 'bool'
-          }
-        ],
-        'payable': false,
-        'stateMutability': 'view',
-        'type': 'function'
-      }
-      ], contractAddress)
-    app.log(contractAddress)
-    var pullRequestId = context.payload.issue.number
-    var repoId = context.payload.repository.full_name
-    var hexRepoId = web3.utils.fromAscii(repoId)
-    contract.methods.isPassing(hexRepoId, pullRequestId).call().then(result => {
+        });
+      });
+
+   //check if repo has been initialized
+    var contract = new web3.eth.Contract(contract_interface, contract_address);
+    contract.methods
+    .repo(hex_repo_id)
+    .call()
+    .then(result => {
       if (result) {
-        app.log(result)
-        var splitRepoId = repoId.split('/')
-        const owner = splitRepoId[0]
-        const repo = splitRepoId[1]
-        const number = pullRequestId
-        context.github.pullRequests.merge({
-          owner,
-          repo,
-          number
-        })
+        if (result[1] == 0) { //voting threshold
+          bodyComment += "\n This repo has not be set up yet. ";
+          bodyComment +=
+          "Please click [here](https://dao-git.github.io/front-end/initialize.html" +
+          "?repo=" +
+          hex_repo_id +
+          "&contract=" +
+          contract_address +
+          ") to initialize.";
+          const noRepoComment = context.issue({ body: bodyComment });
+          return context.github.issues.createComment(noRepoComment);
+        } else {
+          app.log("repo initialized");
+          bodyComment +=
+            "\n Please vote on this pull request [here](https://dao-git.github.io/front-end/" +
+            "?repo=" +
+            hex_repo_id +
+            "&pr=" +
+            pull_request_id +
+            "&contract=" +
+            contract_address +
+            ").";
+          const comment = context.issue({ body: bodyComment });
+          return  context.github.issues.createComment(comment);
+        }
       }
-    })
-  })
-}
+    });
+
+   return;
+  });
+
+
+  //check if voting threshold is passed on every comment
+  app.on("issue_comment.created", async context => {
+    var contract = new web3.eth.Contract(contract_interface, contract_address);
+    app.log(contract_address);
+    var pull_request_id = context.payload.issue.number;
+    var repo_id = context.payload.repository.full_name;
+    var hex_repo_id = web3.utils.fromAscii(repo_id);
+    contract.methods
+      .isPassing(hex_repo_id, pull_request_id)
+      .call()
+      .then(result => {
+        if (result) {
+          app.log(result);
+          var split_repo_id = repo_id.split("/");
+          const owner = split_repo_id[0];
+          const repo = split_repo_id[1];
+          const base = "master";
+          const number = pull_request_id;
+          context.github.pullRequests.merge({
+            owner,
+            repo,
+            number
+          });
+        }
+      });
+  });
+};
